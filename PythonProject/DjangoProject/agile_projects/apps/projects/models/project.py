912aaefd6067f5581c478d3caf36f603d72a7572
ElenaOlena <brovada123g@gmail.com>
2025-07-09 13:28:43 +0200

    Создана модель ProjectFile, проведены миграции

--- PythonProject/DjangoProject/agile_projects/apps/projects/models/project.py
+++ PythonProject/DjangoProject/agile_projects/apps/projects/models/project.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 
+@property
+def count_of_files(self):
+    return self.files.count()
+
+
 class Project(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=False, null=False)
     created = models.DateTimeField(auto_now_add=True)
+    files = models.ManyToManyField('ProjectFile', related_name='projects')
 
     def __str__(self):
         return self.name
